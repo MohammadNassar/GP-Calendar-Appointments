b7358e7b904c4950b894076a3b67057109816776
Mohammad Nassar <safeermohammed13190@yahoo.co.uk>
2015-02-26 23:28:03 +0000

    Retrieving data from the database (table: 'appointments') and printing out results without a GUI.

---
+++ src/DatabaseConnection.java
@@ -0,0 +1,59 @@
+// 'Calendar Appointments' module - By Mohammad Nassar - ID No. 120132108
+
+import java.sql.*;
+
+public class DatabaseConnection {
+    
+    private Connection connect;
+    private Statement statement;
+    private ResultSet result;
+    
+    public DatabaseConnection() {
+        
+        try {
+            // Loading mysql driver
+            Class.forName("com.mysql.jdbc.Driver");
+            
+            // Get/create the connection
+            connect = DriverManager.getConnection("jdbc:mysql://localhost:3306/sega", "root", "");
+            // Create a statement
+            statement = connect.createStatement();
+            
+        } catch (ClassNotFoundException c) {
+            System.out.println("Unable to create the connection. ==> ("+c+")");
+        } catch (SQLException s) {
+            System.out.println("Unable to create statement. ==> ("+s+")");
+        }
+    }
+    
+    // Retrieve records from the database
+    public void getRecords() {
+        
+        String query = "SELECT * FROM Appointments;";
+        
+        try {
+            // Execute the SQL statement and save the returned value in variable 'result'.
+            result = statement.executeQuery(query);
+            
+            while (result.next()) {
+                String appId = result.getString("appId");
+                String appType = result.getString("appType");
+                String patientId = result.getString("patientId");
+                String appWithStaffId = result.getString("appWithStaffId");
+                String date = result.getString("date");
+                String startTime = result.getString("startTime");
+                String finishTime = result.getString("finishTime");
+                
+                // Concatenate results retrieved from table and print them.
+                String rowInTable = "App.No.: "+appId+"\t"+"App.Type is: "+appType+"\t";
+                if (appType.equalsIgnoreCase("Routine")) rowInTable += "\t";
+                rowInTable += "Patient: "+patientId+"\t"+"App.with: "+appWithStaffId+"\t";
+                rowInTable += "Date: "+date+"\t"+"Start time: "+startTime+"\t"+"Finish time: "+finishTime+"\t";
+                System.out.println(rowInTable);
+            }
+            
+        } catch (SQLException s) {
+            System.out.println("Unable to execute query. ==> ("+s+")");
+        }
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,11 @@
+// 'Calendar Appointments' module - By Mohammad Nassar - ID No. 120132108
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        DatabaseConnection connect = new DatabaseConnection();
+        connect.getRecords();
+    }
+    
+}
